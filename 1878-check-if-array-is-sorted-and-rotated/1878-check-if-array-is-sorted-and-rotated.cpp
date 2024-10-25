05538db542487c450d301fe18fc0cb72ca346808
Ashish Kumar Mishra <142574348+coderashu31@users.noreply.github.com>
2024-10-25 17:13:22 +0530

    Time: 0 ms (100%), Space: 10.4 MB (57.58%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,15 +1,38 @@
+// class Solution {
+// public:
+//     bool check(vector<int>& nums) {
+//         vector<int> arr(nums.begin(),nums.end());
+//         sort(arr.begin(),arr.end());
+
+//         for(int i=0;i<nums.size();i++){
+//             if(nums == arr){
+//             return true;
+//             }
+//             rotate(nums.begin(), nums.begin() + 1, nums.end());
+//         }
+//     return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        vector<int> arr(nums.begin(),nums.end());
-        sort(arr.begin(),arr.end());
+        int n=nums.size();
+        int cnt=0;
 
-        for(int i=0;i<nums.size();i++){
-            if(nums == arr){
-            return true;
+        //array is sorted
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i]){
+                cnt++;
             }
-            rotate(nums.begin(), nums.begin() + 1, nums.end());
         }
-    return false;
+
+        //array is rotated
+        if(nums[n-1]>nums[0]){
+            cnt++;
+        }
+
+        return cnt<=1;
     }
 };
