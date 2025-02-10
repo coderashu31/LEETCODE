da898420e802b0dab32f65929569d526503a595f
Ashish Kumar Mishra <142574348+coderashu31@users.noreply.github.com>
2025-02-10 20:02:16 +0530

    Time: 0 ms (100%), Space: 8.9 MB (34.7%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        char ch;
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s[i];
+            if(ch == '(' || ch == '[' || ch == '{'){
+            // got open bracket
+            st.push(ch);
+        } else {
+            if(!st.empty()){
+                // got closed bracket
+                if(ch == ')' && st.top() == '('){
+                    st.pop();
+                } else if (ch == ']' && st.top() == '['){
+                    st.pop();
+                } else if (ch == '}' && st.top() == '{'){
+                    st.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                // stack is empty
+                return false;
+            }
+        }
+        }
+
+        
+        if (st.size() == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
