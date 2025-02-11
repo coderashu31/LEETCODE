0d9959a3a928742e6f89fc1683ff2592ed7b2401
Ashish Kumar Mishra <142574348+coderashu31@users.noreply.github.com>
2025-02-11 20:49:53 +0530

    Time: 0 ms (100%), Space: 12.8 MB (52.07%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-
-    void nge(vector<int>& nums1, vector<int>& nums2, int i, int j,vector<int>& ans){
-        int k;
-        for(k=i+1 ; k< nums2.size(); k++){
-            if(nums2[k] > nums1[j]){
-                ans.push_back(nums2[k]);
-                break;
-            }
-        }
-        if(k == nums2.size()){
-            ans.push_back(-1);
-        }
-    }
-
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> mp;
+        int n = nums2.size();
+        stack<int> st;
         vector<int> ans;
-
-        for(int j=0; j<nums1.size(); j++){
-            for(int i=0; i<nums2.size(); i++){
-                if(nums2[i] == nums1[j]){
-                    nge(nums1, nums2, i, j, ans);
+        for(int i=n-1; i>=0; i--){
+            if(st.empty()){
+                st.push(nums2[i]);
+                mp[nums2[i]] = -1;
+            } 
+            else {
+                while(!st.empty() && st.top()<nums2[i]){
+                
+                        st.pop();
                 }
+                if(st.empty()){
+                    mp[nums2[i]] = -1;
+                } 
+                else {
+                    mp[nums2[i]] = st.top();
+                } 
+                st.push(nums2[i]);
             }
+           
+        }
+        for(int i=0; i<nums1.size();i++){
+            ans.push_back(mp[nums1[i]]);
         }
         return ans;
     }
